94d5a935171cb3346cad6fefecf5035c39219ae1
ZaidBinRashid <zaidbinrashid85@gmail.com>
2024-11-05 20:03:47 +0530

    Added server side rendering using EJS

--- URL-shortener/app.js
+++ URL-shortener/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDb } = require("./connect");
 const urlRoute = require("./routes/Url");
+const staticRoute = require("./routes/staticRoute")
 const URL = require('./models/Url');
 const app = express();
 const PORT = 8001;
@@ -9,10 +11,16 @@ connectToMongoDb("mongodb://localhost:27017/short-url").then(() =>
   console.log("MongoDb Connected")
 );
 
-app.use(express.json());
-app.use("/url", urlRoute);
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views"))
 
-app.get('/:shortId', async (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.use("/url", urlRoute);
+app.use("/", staticRoute)
+
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
