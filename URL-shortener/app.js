e7c3770ff64ce632dcbe55d488e9fa27c4f71186
Zaid Bin Rashid <you@example.com>
2025-03-18 22:37:31 +0530

    added authentication

--- URL-shortener/app.js
+++ URL-shortener/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const path = require("path");
 const { connectToMongoDb } = require("./connect");
-const urlRoute = require("./routes/Url");
-const staticRoute = require("./routes/staticRoute")
+const { restrictToLoggedinUserOnly, checkAuth } = require('./middlewares/auth');
 const URL = require('./models/Url');
 const app = express();
 const PORT = 8001;
+
+const urlRoute = require("./routes/Url");
+const staticRoute = require("./routes/staticRoute");
+const userRoute = require("./routes/user");
+const cookieParser = require("cookie-parser");
+
 
 connectToMongoDb("mongodb://localhost:27017/short-url").then(() =>
   console.log("MongoDb Connected")
@@ -16,9 +21,12 @@ app.set('views', path.resolve("./views"))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
 
-app.use("/url", urlRoute);
-app.use("/", staticRoute)
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user",userRoute);
+app.use("/", checkAuth, staticRoute);
+
 
 app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
